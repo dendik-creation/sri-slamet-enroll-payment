430ca059d4aec683d2713faae8b23c6544223599
dendik-creation <setiawandendik0205@gmail.com>
2026-02-27 21:36:00 +0700

    [fix] adjust thr caclucate formula for employee daily join_date under 1 month from now

--- app/Http/Controllers/ThrController.php
+++ app/Http/Controllers/ThrController.php
@@ -25,107 +25,144 @@ class ThrController extends Controller
         }
     }
 
-    public function index(){
-        $thr_bonuses = ThrBonus::with('employee_thrs')->paginate(10);
-        $is_thr_given = ThrBonus::where('year', date('Y'))->exists();
-        return Inertia::render('ThrBonus/Index', [
-            'title' => "THR Karyawan",
-            'description' => "Daftar THR tahunan untuk karyawan",
-            'thr_bonuses' => $thr_bonuses,
-            'is_thr_given' => $is_thr_given
+    public function index()
+    {
+        $thr_bonuses = ThrBonus::with("employee_thrs")->paginate(10);
+        $is_thr_given = ThrBonus::where("year", date("Y"))->exists();
+        return Inertia::render("ThrBonus/Index", [
+            "title" => "THR Karyawan",
+            "description" => "Daftar THR tahunan untuk karyawan",
+            "thr_bonuses" => $thr_bonuses,
+            "is_thr_given" => $is_thr_given,
         ]);
     }
 
-    public function show($thr_bonus_id){
-        $thr_bonus = ThrBonus::with('employee_thrs.employee')->findOrFail($thr_bonus_id);
-        return Inertia::render('ThrBonus/Show', [
-            'title' => "Detail THR Karyawan Tahun {$thr_bonus->year}",
-            'description' => "Anda dapat melakukan cetak secara keseluruhan atau sebagian",
-            'thr_bonus' => $thr_bonus
+    public function show($thr_bonus_id)
+    {
+        $thr_bonus = ThrBonus::with("employee_thrs.employee")->findOrFail(
+            $thr_bonus_id,
+        );
+        return Inertia::render("ThrBonus/Show", [
+            "title" => "Detail THR Karyawan Tahun {$thr_bonus->year}",
+            "description" =>
+                "Anda dapat melakukan cetak secara keseluruhan atau sebagian",
+            "thr_bonus" => $thr_bonus,
         ]);
     }
 
-    private function calculateTHRFormula($salary_per_day, $salary_type, $join_date, $salary_per_month = 0){
-        if ($salary_type === 'monthly') {
+    private function calculateTHRFormula(
+        $salary_per_day,
+        $salary_type,
+        $join_date,
+        $salary_per_month = 0,
+    ) {
+        if ($salary_type === "monthly") {
             return $salary_per_month;
         }
-        
-        $join_date = Carbon::parse($join_date);
-        $now = Carbon::now();
-        $months_worked = floor($join_date->diffInMonths($now));
-        
+
+        $join_date = Carbon::parse($join_date)->startOfDay();
+        $now = Carbon::now()->startOfDay();
+
+        $total_days = $join_date->diffInDays($now) + 1;
+        if ($total_days < 30) {
+            return (30 * $salary_per_day) / 12;
+        }
+
+        $months_worked = $total_days / 30;
+
         if ($months_worked >= 12) {
             return 30 * $salary_per_day;
         } else {
-            return $months_worked == 0 ? $salary_per_day : $months_worked * $salary_per_day;
+            return ($months_worked * (30 * $salary_per_day)) / 12;
         }
     }
-    
-    private function calculateTHR(){
-        $employees = Employee::get(['id', 'salary_per_day', 'salary_per_month', 'salary_type', 'join_date']);
+
+    private function calculateTHR()
+    {
+        $employees = Employee::get([
+            "id",
+            "salary_per_day",
+            "salary_per_month",
+            "salary_type",
+            "join_date",
+        ]);
         $thr_data = [];
         foreach ($employees as $employee) {
-            $amount = $this->calculateTHRFormula($employee['salary_per_day'], $employee['salary_type'], $employee['join_date'], $employee['salary_per_month']);
+            $amount = $this->calculateTHRFormula(
+                $employee["salary_per_day"],
+                $employee["salary_type"],
+                $employee["join_date"],
+                $employee["salary_per_month"],
+            );
             $thr_data[] = [
-                'employee_id' => $employee['id'],
-                'amount' => $this->convertToRoundBonus($amount),
-                'created_at' => now(),
-                'updated_at' => now()
+                "employee_id" => $employee["id"],
+                "amount" => $this->convertToRoundBonus($amount),
+                "created_at" => now(),
+                "updated_at" => now(),
             ];
         }
         return $thr_data;
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
-            'year' => 'required',
+            "year" => "required",
         ]);
-        $year = $request->input('year', date('Y'));
+        $year = $request->input("year", date("Y"));
         $thr_data = $this->calculateTHR();
-        $accumulated_amount = array_sum(array_column($thr_data, 'amount'));
+        $accumulated_amount = array_sum(array_column($thr_data, "amount"));
         $thr_bonus = ThrBonus::create([
-            'year' => $year,
-            'paid_at' => now()->format('Y-m-d'),
-            'total_amount' => $this->convertToRoundBonus($accumulated_amount)
+            "year" => $year,
+            "paid_at" => now()->format("Y-m-d"),
+            "total_amount" => $this->convertToRoundBonus($accumulated_amount),
         ]);
         foreach ($thr_data as &$data) {
-            $data['thr_bonus_id'] = $thr_bonus->id;
+            $data["thr_bonus_id"] = $thr_bonus->id;
         }
         $thr_bonus->employee_thrs()->createMany($thr_data);
-        Session::flash('success', 'THR berhasil dibuat untuk tahun ' . $year);
-        return Inertia::location('/thr');
+        Session::flash("success", "THR berhasil dibuat untuk tahun " . $year);
+        return Inertia::location("/thr");
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
         $thr_bonus = ThrBonus::findOrFail($id);
         $thr_bonus->employee_thrs()->delete();
         $thr_bonus->delete();
-        Session::flash('success', 'THR berhasil dihapus.');
-        return Inertia::location('/thr');
+        Session::flash("success", "THR berhasil dihapus.");
+        return Inertia::location("/thr");
     }
 
-    public function printAll($thr_bonus_id){
-        $thr_bonus = ThrBonus::with('employee_thrs.employee.position')->findOrFail($thr_bonus_id);
-        return Inertia::render('ThrBonus/PrintAll', [
-            'title' => "Cetak THR Karyawan Tahun {$thr_bonus->year}",
-            'description' => "Cetak semua THR karyawan untuk tahun {$thr_bonus->year}",
-            'thr_bonus' => $thr_bonus,
-            'company_name' => "CV Sri Slamet",
-            'back_url' => "/thr/{$thr_bonus_id}"
+    public function printAll($thr_bonus_id)
+    {
+        $thr_bonus = ThrBonus::with(
+            "employee_thrs.employee.position",
+        )->findOrFail($thr_bonus_id);
+        return Inertia::render("ThrBonus/PrintAll", [
+            "title" => "Cetak THR Karyawan Tahun {$thr_bonus->year}",
+            "description" => "Cetak semua THR karyawan untuk tahun {$thr_bonus->year}",
+            "thr_bonus" => $thr_bonus,
+            "company_name" => "CV Sri Slamet",
+            "back_url" => "/thr/{$thr_bonus_id}",
         ]);
     }
-    
-    public function printSpesific($thr_bonus_id, $employee_thr_id){
-        $employee_thr = EmployeeThrBonus::with('employee.position', 'thr_bonus')->findOrFail($employee_thr_id);
+
+    public function printSpesific($thr_bonus_id, $employee_thr_id)
+    {
+        $employee_thr = EmployeeThrBonus::with(
+            "employee.position",
+            "thr_bonus",
+        )->findOrFail($employee_thr_id);
         if (!$employee_thr) {
-            abort(404, 'Data THR karyawan tidak ditemukan.');
+            abort(404, "Data THR karyawan tidak ditemukan.");
         }
-        return Inertia::render('ThrBonus/PrintSpesific', [
-            'title' => "Cetak THR Karyawan {$employee_thr->employee->name} Tahun {$employee_thr->thr_bonus->year}",
-            'description' => "Cetak THR spesifik untuk karyawan {$employee_thr->employee->name}",
-            'employee_thr' => $employee_thr,
-            'company_name' => "CV Sri Slamet",
-            'back_url' => "/thr/{$thr_bonus_id}"
+        return Inertia::render("ThrBonus/PrintSpesific", [
+            "title" => "Cetak THR Karyawan {$employee_thr->employee->name} Tahun {$employee_thr->thr_bonus->year}",
+            "description" => "Cetak THR spesifik untuk karyawan {$employee_thr->employee->name}",
+            "employee_thr" => $employee_thr,
+            "company_name" => "CV Sri Slamet",
+            "back_url" => "/thr/{$thr_bonus_id}",
         ]);
     }
 }
